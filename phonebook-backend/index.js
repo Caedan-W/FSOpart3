49bde05c4e78c7a46144d2bf910b24e85d1598e0
Caedan-W <421155149@qq.com>
2024-04-21 15:09:36 -0700

    Exercises 3.9-3.11 completed; deploy to fly.io;

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 // 自定义 token 函数，用于记录请求体
 morgan.token('req-body', (req, res) => JSON.stringify(req.body));
@@ -117,7 +120,8 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = 3001
+//const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
