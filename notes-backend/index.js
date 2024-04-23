233b946e772e30010c3cc1e8f71b7251610ed692
Caedan-W <421155149@qq.com>
2024-04-22 17:47:13 -0700

    Exercises 3.15-3.18 completed; add other operations in phonebook-backend

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -34,65 +34,39 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-        console.log(notes)
-    })
+app.get('/api/notes', (request, response, next) => {
+    Note.find({})
+        .then(notes => {
+            response.json(notes)
+            console.log(notes)
+        })
+        .catch(error => next(error))
 })
 
-/*
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)    
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
-})
-*/
 
-/*
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
-})
-*/
-
-app.get('/api/notes/:id', (request, response) => {
-    //console.log('request params:',request.params)
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
                 response.json(note)
             } else {
-                //response.status(404).json({ error: 'Note not found' })
                 response.status(404).end()
             }
         })
-        .catch(error => {
-            console.error('Error fetching note:', error)
-            response.status(500).json({ error: 'Internal server error' })
+        .catch(error => next(error))
+})
+
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
         })
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
 
-    response.status(204).end()
-})
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (body.content === undefined) {
@@ -104,9 +78,26 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false,
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
 
@@ -114,3 +105,22 @@ const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } 
+
+    next(error)
+}
+  // this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -46,29 +46,32 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-        console.log(persons)
-    })
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+            console.log(persons)
+        })
+        .catch(error => next(error))
 })
 
 
-app.get('/api/persons/:id', (request, response) => {
-    //const id = Number(request.params.id)
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
                 response.json(person)
         })
+        .catch(error => next(error))
+        /*
         .catch(error => {
             console.error('Error fetching person:', error)
             response.status(404).json({ error: 'person not found' })
         })
-
+        */
 })
 
 
-app.get('/info',(request, response) => {
+app.get('/info',(request, response, next) => {
     const returnedPerson = Person.find({})
     // console.log('typeof Person',typeof Person)
     // console.log('typeof returnedPerson', typeof returnedPerson)
@@ -79,28 +82,21 @@ app.get('/info',(request, response) => {
     returnedPerson.then(person => {
         response.send(`Phonebook has info for ${person.length} people<br><br>${time}`)
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
 
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
-const generateRandomId = () => {
-    return Math.floor(Math.random() * 10000)
-}
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body) {
@@ -120,7 +116,7 @@ app.post('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         if (persons.find(item => item.name === body.name)) {
             return response.status(400).json({ 
-                error: 'name must be unique' 
+                error: `name must be unique, the number of ${body.name} will be updated` 
             })
         }
         // 如果名字是唯一的，继续处理 POST 请求...
@@ -133,8 +129,25 @@ app.post('/api/persons', (request, response) => {
             response.json(savedPerson)
         })
     })
+    .catch(error => next(error))
 })
 
+
+// Update phonebook entry by ID
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
 
 
 const PORT = process.env.PORT
@@ -148,3 +161,15 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } 
+
+    next(error)
+}
+  // this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
